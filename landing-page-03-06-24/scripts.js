ea4494c05ce02b596c9077569c6c61af18191abe
Sanabio <76131493+sanabio100@users.noreply.github.com>
2024-06-26 09:37:19 -0300

    Delete landing-page-03-06-24 directory

--- landing-page-03-06-24/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        alert('Mensagem enviada com sucesso!');
-    });
-});
